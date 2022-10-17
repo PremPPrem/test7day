d42e97716ea6a19299d656cc20a96cf5ae2e6d02
PremPPrem <108279151+PremPPrem@users.noreply.github.com>
2022-10-18 03:12:38 +0700

    Update AfterLogin.vue

--- src/views/AfterLogin.vue
+++ src/views/AfterLogin.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="after-login">
-    <a href="">home</a>
-    <a href="">add</a>
-    <a href="">update</a>
     <!-- <a href="#login">Logout</a> -->
-    <router-link to="/login">Logout</router-link>
-    <div class="add-data">
-      <input v-model="id" type="text" placeholder="Enter Id" name="id" />
-      <input v-model="email" type="text" placeholder="Enter Email" name="email" />
-      <input v-model="firstName" type="text" placeholder="Enter First Name" name="First Name" />
-      <input v-model="lastName" type="text" placeholder="Enter Last Name" name="Last Name" />
-      <input  type="file" accept="image/png, image/jpeg" />
-      <button @click="addData">Add</button>
-    </div>
+    <router-link to="/login" class="ctn btn-logout">Logout</router-link>
+    
     <table>
       <tr>
         <th>id</th>
@@ -20,7 +10,6 @@
         <th>email</th>
         <th>first name</th>
         <th>last name</th>
-        <th>Edit</th>
       </tr>
       <tr v-for="item in list" :key="item.id">
         <td>{{ item.id }}</td>
@@ -28,52 +17,25 @@
         <td>{{ item.email }}</td>
         <td>{{ item.first_name }}</td>
         <td>{{ item.last_name }}</td>
-        <td></td>
       </tr>
     </table>
+
   </div>
-  <Footer></Footer>
 </template>
 
 <script>
-
-
 import axios from "axios";
-
+const baseUrl = "https://reqres.in/api/users";
 export default {
   name: "Home",
-  components: {
-  },
+  components: {},
   data() {
     return {
       list: [],
-      add: {
-        id: "",
-    avatar: "",
-    email: "",
-    firstName: "",
-    lastName: "",
-        
-      }
+     
     };
   },
 
-  methods: {
-    // logout() {
-    //   localStorage.clear();
-    //   this.$router.push({ name: "login" });
-    // },
-    addData(data) {
-        axios.post('https://reqres.in/api/users', {
-name: this.name,
-email:this.email,
-firstName: this.firstName,
-lastName: this.lastName,
-
-        })
-        console.log(data);
-    }
-  },
 
   async mounted() {
     let results = await axios.get("https://reqres.in/api/users");
@@ -99,11 +61,16 @@ lastName: this.lastName,
   }
 
   td {
-    width: 150px;
+    width: 40rem;
     height: 40px;
     font-size: 1rem;
     border: 2px solid $gray;
+    text-align: center;
+
+
   }
+
+  
 
   th {
     font-size: 1.5rem;
@@ -117,31 +84,13 @@ lastName: this.lastName,
   }
 }
 
-.add-data {
-  & input {
-    @include input;
-    margin-bottom: 20px;
-    padding-left: 20px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    height: 40px;
-    width: 300px;
 
-    &:nth-last-child(2) {
-      border: none;
-    }
-  }
-
-  & button {
-    @include button-ctn;
-    width: 320px;
-    height: 40px;
-    margin-bottom: 20px;
-    padding-left: 20px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
+.btn-logout {
+ display: block;;
+  margin: 30px;
+  text-align: center;
+  justify-items: center;
+  align-items: center;
 }
+
 </style>
